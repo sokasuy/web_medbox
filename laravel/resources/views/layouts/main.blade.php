b8d50971726cbf9fd9c204c9a8b5698d784b3689
Yusak OS <airantou2@gmail.com>
2022-10-06 17:05:51 +0700

    update 6 oktober jam 5.05 sore sudah auth

--- laravel/resources/views/layouts/main.blade.php
+++ laravel/resources/views/layouts/main.blade.php
@@ -437,27 +437,15 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-table"></i>
               <p>
-                Tables
+                Reports
                 <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="pages/tables/simple.html" class="nav-link">
+                <a href="{{ route('msbarang.index') }}" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Simple Tables</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/tables/data.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>DataTables</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="pages/tables/jsgrid.html" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>jsGrid</p>
+                  <p>Data Master Barang</p>
                 </a>
               </li>
             </ul>
